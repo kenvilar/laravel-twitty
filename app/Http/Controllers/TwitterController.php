8994d65c520859fa6953dc18312722098796ff8e
Ken Vilar <kenvilar@gmail.com>
2018-03-28 09:54:25 +0800

    Returns a collection of the most recent Tweets limit with 10

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Twitter;
+use File;
 
 class TwitterController extends Controller
 {
     public function userTimeline() {
+	    $tweets = Twitter::getUserTimeline([
+	    	'count' => '10',
+	    ]);
 
+	    return view('twitter')->with('tweets', $tweets);
     }
 
     public function tweet() {
