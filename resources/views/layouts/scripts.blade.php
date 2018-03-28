85f0cb4110c4ff2d20c4797ecaad7451ec536124
Ken Vilar <kenvilar@gmail.com>
2018-03-28 10:09:04 +0800

    Create layout for scripts

---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,5 @@
+<script
+        src="//code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script src="//bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
