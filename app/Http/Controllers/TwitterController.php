ae28f8f26357411906d5f56fd176dee24a371d4f
Ken Vilar <kenvilar@gmail.com>
2018-03-28 09:02:27 +0800

    Create TwitterController

---
+++ app/Http/Controllers/TwitterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TwitterController extends Controller
+{
+    public function userTimeline() {
+
+    }
+
+    public function tweet() {
+
+    }
+}
